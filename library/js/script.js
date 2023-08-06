2a5912d4d5332902f98a9fe1cc5a825ecfaab95d
CerBeer <ocelopp@gmail.com>
2023-08-06 15:53:41 +0300

    refactor: clear, change structure, added footer welcome burger-menu

--- library/js/script.js
+++ library/js/script.js
@@ -4,3 +4,16 @@ let self_esteem = 'Самооценка работы: 100 баллов \n \
 - Вёрстка соответствует макету +54 \n \
 - Общие требования к верстке +20'
 console.log(self_esteem);
+
+(function () {
+    const burgerOpen = document.querySelector('.header-burger-icon');
+    burgerOpen.addEventListener('click', () => {
+        const burgerMenu = document.querySelector('.header-burger-menu');
+        burgerMenu.classList.add('header-burger-menu-active');
+    });
+    const burgerClose = document.querySelector('.header-burger-menu-icon');
+    burgerClose.addEventListener('click', () => {
+        const burgerMenu = document.querySelector('.header-burger-menu');
+        burgerMenu.classList.remove('header-burger-menu-active');
+    });
+}());
