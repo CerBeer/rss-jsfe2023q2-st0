78ecf584bb264cf2a42a9f50341ca748ce838c93
CerBeer <ocelopp@gmail.com>
2023-08-22 23:59:56 +0300

    refactor: change structure nav, remove duplication menu

--- library/js/script.js
+++ library/js/script.js
@@ -1,47 +1,85 @@
 
-const burgerButtonOpen = document.querySelector('.header-burger-button-open');
-const burgerMenu = document.querySelector('.header-burger-menu');
+// #section-start burger-menu
 
-function burgerMenuOpen(e) {
-    e.stopPropagation();
-    if (!burgerMenu.classList.contains('header-burger-menu-active')) {
-        document.addEventListener('click', burgerMenuClose);
-        burgerMenu.classList.add('header-burger-menu-active');
-        burgerButtonOpen.classList.add('header-burger-button-open-hide');
+    const burgerButtonOpen = document.querySelector('.header-burger-button-open');
+    const burgerMenu = document.querySelector('.header-nav');
+
+    function burgerMenuClickOpen(e) {
+        e.stopPropagation();
+        if (!burgerMenu.classList.contains('header-nav-slow-mo')) {
+            burgerMenu.classList.add('header-nav-slow-mo');
+        }
+        if (!burgerMenu.classList.contains('header-burger-menu-active')) {
+            document.addEventListener('click', burgerMenuClickClose);
+            burgerMenu.classList.add('header-burger-menu-active');
+        }
     }
-}
 
-function burgerMenuClose(e) {
-    const target = e.target;
-    //const its_burgerMenu = target == burgerMenu || burgerMenu.contains(target);
-    const its_burgerMenu = target == burgerMenu;
-    if (!its_burgerMenu) {
+    function burgerMenuClickClose(e) {
+        const target = e.target;
+        //const its_burgerMenu = target == burgerMenu || burgerMenu.contains(target);
+        const its_burgerMenu = target == burgerMenu;
+        if (!its_burgerMenu) {
+            burgerMenuClose();
+        }
+    }
+
+    window.addEventListener('resize', function(event) {
+        burgerMenuClose();
+        let docWidth = document.documentElement.offsetWidth;
+        if ((docWidth >= 1030) & (burgerMenu.classList.contains('header-nav-slow-mo'))) {
+            burgerMenu.classList.remove('header-nav-slow-mo');
+        }
+    }, true);
+
+    window.addEventListener('scroll', function(event) {
+        burgerMenuClose();
+    });
+
+
+    function burgerMenuClose() {
         if (burgerMenu.classList.contains('header-burger-menu-active')) {
-            document.removeEventListener('click', burgerMenuClose);
+            document.removeEventListener('click', burgerMenuClickClose);
             burgerMenu.classList.remove('header-burger-menu-active');
-            burgerButtonOpen.classList.remove('header-burger-button-open-hide');
         }
     }
-}
 
-burgerButtonOpen.addEventListener('click', burgerMenuOpen);
+    burgerButtonOpen.addEventListener('click', burgerMenuClickOpen);
 
-let self_esteem = 'Самооценка работы: 50 баллов \n \
-- Вёрстка соответствует макету. Ширина экрана 768px +26 \n \
-- Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12 \n \
-- На ширине экрана 768рх реализовано адаптивное меню +12'
-console.log(self_esteem);
+// #section-end burger-menu
 
-const footerLeftYear = document.querySelector('.find-offset-width');
-function talkOffset(e) {
-    let docWidth = document.documentElement.offsetWidth;
-    console.log('Document width: ' + docWidth);
-    [].forEach.call(
-        document.querySelectorAll('*'),
-        function(el) {
-        if (el.offsetWidth > docWidth) {
-            console.log(el);
+// #section-start self-esteemate
+
+    let self_esteem = 'Самооценка работы: 50 баллов \n \
+    - Вёрстка соответствует макету. Ширина экрана 768px +26 \n \
+    - Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12 \n \
+    - На ширине экрана 768рх реализовано адаптивное меню +12'
+    console.log(self_esteem);
+
+// #section-end self-esteemate
+
+// #section-start find-owersized-elements
+
+    function talkOffset(e) {
+        let docWidth = document.documentElement.offsetWidth;
+        console.log('Document width: ' + docWidth);
+        [].forEach.call(
+            document.querySelectorAll('*'),
+            function(el) {
+            if (el.offsetWidth > docWidth) {
+                console.log(el);
+            }
         }
-    }
-)};
-footerLeftYear.addEventListener('click', talkOffset);
+    )};
+
+    const findOffsetWidth = document.querySelector('.find-offset-width');
+    if(findOffsetWidth === null) console.error(`Not fount selector find-offset-width`);
+        else footerLeftYear.addEventListener('click', talkOffset);
+
+// #section-end find-owersized-elements
+
+// #section-start carousel
+
+
+
+// #section-end carousel
