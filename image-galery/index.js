3c0a6700c20e5dbb15828fe49071a550287e5fa2
CerBeer <ocelopp@gmail.com>
2023-09-29 00:44:35 +0300

    feat: added markup

---
+++ image-galery/index.js
@@ -0,0 +1,24 @@
+
+
+
+// #self-esteemate
+
+let self_esteem = 'Self-assessment of work: 70 points \n \
+\n \
+1. Layout +10 \n \
+ + audio player layout: there is a Play/Pause button, “Forward” and “Back” buttons for scrolling through audio tracks, a progress bar, displaying the title and author of the track +5 \n \
+ + in the footer of the application there is a link to the GitHub of the author of the application, the year the application was created, the course logo with a link to the course +5 \n \
+2. Play/Pause button +10 \n \
+ + there is a Play/Pause button, when you click on it you can start or stop playing an audio track +5 \n \
+ + The appearance and functionality of the Play/Pause button changes depending on whether an audio track is currently playing +5 \n \
+3. Pressing the “Forward” and “Back” buttons switches the audio track being played. Audio tracks are scrolled in a circle - after the last one comes the first +10 \n \
+4. When changing the audio track, the image changes - audio track cover +10 \n \
+5. The progress bar displays the progress of playing a modern audio track. When moving the slider manually, it changes the current playing time of the audio track +10 \n \
+6. Displays the duration of the audio track and its current playing time +10 \n \
+7. Additional functionality not provided for in the task that improves the quality of the application +10 \n \
+ + Added volume control. \n \
+ + Added saving of the last playing track and volume level when reloading the page.';
+console.log(self_esteem);
+
+// #self-esteemate
+
