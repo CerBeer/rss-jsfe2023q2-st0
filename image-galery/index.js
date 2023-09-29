56f5634e2c28c2547df911a732edcaea0598ea9a
CerBeer <ocelopp@gmail.com>
2023-09-29 23:51:37 +0300

    feat: Implemented receiving images by search query

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,14 +1,20 @@
 const image_gallery_main_container = document.querySelector('#image-gallery-main-container');
+const searchBoxInput = document.querySelector('#image-gallery-header-search-box-input');
+const searchBoxLens = document.querySelector('#image-gallery-header-search-box-lens');
 
-function searchImage(query = '') {
+const API_KEY = "zMp_vKYIaLpsS44S2nU7oUPgYO1HicPI1zgtQIZo5sk";
+
+function searchImage() {
 
     image_gallery_main_container.innerHTML = '';
 
-    if (query === '') {
+    let query = searchBoxInput.value;
+    console.log('query', query);
+    if (query.length == 0) {
         getTenFreeCats();
         getTenFreeDogs();
     } else {
-        fetchQuery(query);
+        getUnsplashImages(query);
     }
 }
 
@@ -28,21 +34,65 @@ async function fetchQuery(url) {
     const data = await response.json();
     console.log('data', data);
 
+    if (data.length > 0) {
+
+    }
+
     addImages(data);
 }
 
 function addImages(results) {
 
     results.map(result => {
-        const imageWrapper = document.createElement('div');
-        imageWrapper.classList.add('image-gallery-main-container-image');
+        const imageBox = document.createElement('div');
+        imageBox.classList.add('image-gallery-main-container-image');
         const image = document.createElement('img');
         image.src = result.url;
         image.alt = result.id;
 
-        imageWrapper.appendChild(image);
-        image_gallery_main_container.appendChild(imageWrapper);
+        imageBox.appendChild(image);
+        image_gallery_main_container.appendChild(imageBox);
     });
 
 }
 
+function getUnsplashImages(query) {
+    url = `https://api.unsplash.com/search/photos?query=${query}&per_page=12&tag_mode=all&orientation=landscape&client_id=${API_KEY}`;
+    fetchUnsplashQuery(url);
+}
+
+async function fetchUnsplashQuery(url) {
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log('data', data);
+    const answer = data.results;
+
+    if (answer.length > 0) {
+        addUnsplashImages(answer);
+    } else {
+        getTenFreeCats();
+        getTenFreeDogs();
+    }
+}
+
+function addUnsplashImages(results) {
+
+    results.map(result => {
+        const imageBox = document.createElement('div');
+        imageBox.classList.add('image-gallery-main-container-image');
+        const image = document.createElement('img');
+        image.src = result.urls.regular;
+        image.alt = result.alt_description;
+
+        imageBox.appendChild(image);
+        image_gallery_main_container.appendChild(imageBox);
+    });
+
+}
+
+searchBoxInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') searchImage();
+})
+
+searchBoxLens.addEventListener('click', searchImage);
