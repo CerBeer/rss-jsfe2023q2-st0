64528421c83cf31c607468ba3695ef32a988dee8
CerBeer <ocelopp@gmail.com>
2023-09-30 23:53:23 +0300

    refactor: code refactoring, boilerprint removal

--- image-galery/selfesteem.js
+++ image-galery/selfesteem.js
@@ -1,20 +1,22 @@
 // #self-esteemate
 
-let self_esteem = 'Self-assessment of work: 70 points \n \
+let self_esteem = "Self-assessment of work: 70 out of 60 points\n \
 \n \
 1. Layout +10 \n \
- + audio player layout: there is a Play/Pause button, “Forward” and “Back” buttons for scrolling through audio tracks, a progress bar, displaying the title and author of the track +5 \n \
+ + there are several photos on the page and a search bar +5 \n \
  + in the footer of the application there is a link to the GitHub of the author of the application, the year the application was created, the course logo with a link to the course +5 \n \
-2. Play/Pause button +10 \n \
- + there is a Play/Pause button, when you click on it you can start or stop playing an audio track +5 \n \
- + The appearance and functionality of the Play/Pause button changes depending on whether an audio track is currently playing +5 \n \
-3. Pressing the “Forward” and “Back” buttons switches the audio track being played. Audio tracks are scrolled in a circle - after the last one comes the first +10 \n \
-4. When changing the audio track, the image changes - audio track cover +10 \n \
-5. The progress bar displays the progress of playing a modern audio track. When moving the slider manually, it changes the current playing time of the audio track +10 \n \
-6. Displays the duration of the audio track and its current playing time +10 \n \
-7. Additional functionality not provided for in the task that improves the quality of the application +10 \n \
- + Added volume control. \n \
- + Added saving of the last playing track and volume level when reloading the page.';
+2. When loading the application, images received from the API are displayed on the page +10 \n \
+3. If you enter a word in the search field and submit a search query, the page will display images of the relevant subject if such data is provided by the API +10 \n \
+4. Search +30 \n \
+ + when opening the application, the cursor is in the input field +5 \n \
+ + there is placeholder +5 \n \
+ + autocomplete of the input field is disabled (no drop-down list with previous queries) +5 \n \
+ + a search query can be sent by pressing Enter +5 \n \
+ + after submitting a search query and displaying search results, the search query continues to be displayed in the input field +5 \n \
+ + in the input field there is a cross when clicked on which the search query from the input field is removed and placeholder is displayed +5 \n \
+5. Additional functionality, not provided for in the task that improves the quality of the application +10 \n \
+ + added zoom image. \n \
+ + added mock API request when unsplash doesn't provide images";
 console.log(self_esteem);
 
 // #self-esteemate
